b3cff072cccb4b1286692aaf2e6b59f89c2812f0
иван <52652515+nomadroom@users.noreply.github.com>
2021-02-02 21:04:25 +0300

    Домашняя работа
    Домашняя работа

--- Lesson_1/D_2_and_3.js
+++ Lesson_1/D_2_and_3.js
@@ -1,3 +1,3 @@
 let admin = 'Alex Stone'; 	// Передаем в переменную admin строковое значение 'Alex Stone';
-let name = admin; 		   // Присваиваем переменной name значение переменной admin; 
+let name = admin; 		    // Присваиваем переменной name значение переменной admin; 
 console.log(name);		  // Вывод результата в консоль;
